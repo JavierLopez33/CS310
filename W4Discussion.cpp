74a4ad1330ecdc5f733ba4f9014ae6e39bd27320
javierlf33 <javierlf33@gmail.com>
2024-09-19 22:19:53 -0500

    Week 4 Discussion

---
+++ W4Discussion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numberOfLoops;
+
+    // Ask the user for the number of times to loop
+    cout << "Enter the number of times to loop: ";
+    cin >> numberOfLoops;
+
+    // Loop the specified number of times
+    for (int i = 1; j <= numberOfLoops; i++) {
+        cout << "This is loop number: " << I << endl;
+    }
+
+    return 0;
+}
