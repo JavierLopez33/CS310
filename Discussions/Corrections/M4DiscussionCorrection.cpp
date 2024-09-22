11fdb137bf02269fc260feff402a83cce2a091d8
javierlf33 <javierlf33@gmail.com>
2024-09-22 11:32:17 -0500

    M4 Correction

---
+++ Discussions/Corrections/M4DiscussionCorrection.cpp
@@ -0,0 +1,20 @@
+/*
+    Created by Javier Correcting Boris Davis post
+    File Name:M4_Discussion
+    Date: 9/22/2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int z = 0;
+
+    for (z = 0; z <= 20; z = z + 5)
+
+        cout << z << " ";
+
+    return 0;
+}
