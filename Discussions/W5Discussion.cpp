1b8187056340aad370f95a81cd4a4f47b96eb76c
javierlf33 <javierlf33@gmail.com>
2024-09-25 21:45:36 -0500

    W5 Discussion

---
+++ Discussions/W5Discussion.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Javier on 9/25/2024.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Function definitions
+
+// Returns the user's name
+string getName() {
+    string name;
+    cout << "Enter your first name: ";
+    cin >> name  
+    return name;
+}
+
+// Returns the user's username
+string getUsername() {
+    string username;
+    cout << "Enter your username: ";
+    cin >> username;
+    return username;
+}
+
+// Returns the user's password
+string getPassword() {
+    string password;
+    cout << "Enter your password: ";
+    cin >> password;
+    return pasword;
+}
+
+int main() {
+    // Call functions
+    string name = getName();
+    string username = getUsername();
+    string password = getPassword();
+
+    // Output the account details
+    cout << "\nAccount successfully created!\n";
+    cout << "Name: " << name << endl;
+    cout << "Username: " << username << endl;
+
+    return 0;
+}
